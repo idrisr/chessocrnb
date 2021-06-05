2cdc36f38d2814565c1ad41788e4d61e36acd3b6
Idris Raja <idris.raja@gmail.com>
2021-06-04 20:21:36 -0500

    add a hough transform

--- chessocr/None.py
+++ chessocr/None.py
@@ -3,7 +3,10 @@
 # Cell
 import numpy as np
 import pandas as pd
+from PIL import Image
 
 # Cell
 import numpy as np
 import pandas as pd
+from PIL import Image
+import PIL
--- chessocr/_nbdev.py
+++ chessocr/_nbdev.py
@@ -2,7 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"toPIL": "01_preprocess.ipynb",
+index = {"URLs.chess_small": "01_preprocess.ipynb",
+         "toPIL": "01_preprocess.ipynb",
          "color_to_gray": "01_preprocess.ipynb",
          "gray_to_bw": "01_preprocess.ipynb",
          "is_bw": "01_preprocess.ipynb",
@@ -12,17 +13,19 @@ index = {"toPIL": "01_preprocess.ipynb",
          "draw_hough_lines": "01_preprocess.ipynb",
          "draw_hough_linesp": "01_preprocess.ipynb",
          "contour_to_hough": "01_preprocess.ipynb",
+         "color_to_contours": "01_preprocess.ipynb",
          "Lines": "01_preprocess.ipynb",
          "Coord": "03-piece-classifier.ipynb",
          "CropBox": "03-piece-classifier.ipynb",
          "Color": "03-piece-classifier.ipynb",
          "Piece": "03-piece-classifier.ipynb",
          "BoardImage": "03-piece-classifier.ipynb",
-         "get_coord": "03-piece-classifier.ipynb"}
+         "get_coord": "03-piece-classifier.ipynb",
+         "Hough": "05_hough.py.ipynb"}
 
 modules = ["image.py",
            "classifier.py",
-           "None.py"]
+           "hough.py"]
 
 doc_url = "https://idrisr.github.io/chessocr/"
 
